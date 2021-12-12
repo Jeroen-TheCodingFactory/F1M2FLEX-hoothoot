38dc6255412340803b1bcbb92b731b02be1ff140
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2021-12-12 15:47:05 +0100

    made it so that hoothoot opens his eyes and does his cry, otherwise he goes to sleep. on default hoothoot sleeps

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-document.getElementById("js--hoothoot-belly").onclick = function(event){
-    document.getElementById("js--cry").play();    
+/* by default eyes are closed */
+let checkbox = document.getElementById("hoothoot__checkbox");
+checkbox.checked = false; 
+
+let label = document.getElementById("hoothoot__label");
+label.onclick = function(){
+    if(checkbox.checked === false){
+        document.getElementById("hoothoot__cry").play();
+    }
 }
