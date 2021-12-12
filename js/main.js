94675d5207848d48752d2d421c233de65ba55e6b
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2021-12-12 16:17:20 +0100

    changed let to const

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /* by default eyes are closed */
-let checkbox = document.getElementById("hoothoot__checkbox");
+const checkbox = document.getElementById("hoothoot__checkbox");
 checkbox.checked = false; 
 
-let label = document.getElementById("hoothoot__label");
+const label = document.getElementById("hoothoot__label");
 label.onclick = function(){
     if(checkbox.checked === false){
         document.getElementById("hoothoot__cry").play();
